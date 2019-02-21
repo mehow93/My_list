d9bd4f2314dabfd48f0a5af948ce93263622a6f3
mehow93 <michmad93@gmail.com'git config --global user.email michmad93@gmail.comdsfbvsecho penisgit config --global user.email "michmad93@gmail.com>
2019-02-21 11:28:25 +0100

    commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class List {
+public:
+    struct Node {
+      int var;
+      Node* pNext;
+
+    };
+private:
+
+};
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
