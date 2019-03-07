98ab9868e60d1a09b2643a651d992feaa936637a
mehow93 <michmad93@gmail.com'git config --global user.email michmad93@gmail.comdsfbvsecho penisgit config --global user.email "michmad93@gmail.com>
2019-03-07 12:25:12 +0100

    push() function added

--- main.cpp
+++ main.cpp
@@ -7,18 +7,38 @@ public:
     struct Node {
       int var;
       Node* pNext;
+      Node (int val, Node* pn) : var{val}, pNext{pn}{}
 
     };
     List() :HEAD{nullptr}, size_{0} {}
+    void push(int val);
     int getSize(){return size_;}
 private:
   Node* HEAD = nullptr;
   int size_ =0;
 };
+
+void List::push(int val){
+    Node* pNode =HEAD;
+    if(pNode != nullptr)
+    {
+       while(pNode->pNext != nullptr)
+       {
+           pNode = pNode->pNext;
+       }
+       pNode->pNext = new Node(val, nullptr);
+    }
+    else{
+ HEAD = new Node (val, nullptr);
+    }
+    size_++;
+}
 int main()
 {
     List l1;
-
+     l1.push(1);
+     l1.push(2);
+     l1.push(3);
     cout << l1.getSize() << endl;
     return 0;
 }
