590713c06ecc1828fbb625e6b3c5602ceeb68577
mehow93 <michmad93@gmail.com'git config --global user.email michmad93@gmail.comdsfbvsecho penisgit config --global user.email "michmad93@gmail.com>
2019-04-01 21:40:31 +0200

    added show() and get_size()

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ public:
         Node (int val, Node* pb, Node* pn ) : var{val}, pBefore{pb}, pNext{pn} {}};
     List() :HEAD{nullptr}, size_{0} {}
     void push_back(int val);
+    void show();
+    int get_size(){return size_;}
 
 private:
     Node* HEAD = nullptr;
@@ -23,19 +25,35 @@ void List::push_back(int val){// pushing new values to end of list
     {
         while(pNode->pNext != nullptr)
         {
-       pNode = pNode->pNext;
+            pNode = pNode->pNext;
         }
-       pNode->pNext = new Node(val,pNode,nullptr);
+        pNode->pNext = new Node(val,pNode,nullptr);
     }
     else
     {
         HEAD = new Node(val,nullptr,nullptr);
     }
+    size_++;
+}
 
+void List::show(){
+    Node *pNode = HEAD;
+    while(pNode != nullptr)
+    {
+        cout << "The value is: "<<pNode->var <<endl;
+        pNode = pNode->pNext;
+    }
 
 }
+
 int main()
 {
+     List l1;
+     l1.push_back(3);
+     l1.push_back(4);
+     l1.push_back(12);
+     cout << l1.get_size()<<endl;
+     l1.show();
 
 
 
